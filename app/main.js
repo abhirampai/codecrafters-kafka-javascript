fcaed61b43f898a1a5cc53c674439c87403636ad
Abhiram Pai <abhirampai1999@gmail.com>
2024-11-01 08:17:22 +0530

    Passing stage 'Parse correlation id'

--- app/main.js
+++ app/main.js
@@ -1,12 +1,39 @@
 import net from "net";
 
-const server = net.createServer((connection) => {
-  const responseMessage = {
-    messageSize: Buffer.from(new Array(4).fill(0)),
-    correlationId: Buffer.from([...new Array(3).fill(0), 7])
-  }
+const pick = (obj, ...args) => ({
+  ...args.reduce((res, key) => ({ ...res, [key]: obj[key] }), { })
+})
 
-  connection.write(Buffer.concat(Object.values(responseMessage)));
+const sendResponseMessage = (connection, messageObj) => {
+  return connection.write(Buffer.concat(Object.values(messageObj)));
+};
+
+const server = net.createServer((connection) => {
+  connection.on("data", (buffer) => {
+    const messageSize = buffer.subarray(0, 4);
+    const requestApiKey = buffer.subarray(4, 6);
+    const requestApiVersion = buffer.subarray(6, 8);
+    const correlationId = buffer.subarray(8, 12);
+
+    const responseMessage = {
+      messageSize,
+      requestApiKey,
+      requestApiVersion,
+      correlationId,
+    };
+
+    return sendResponseMessage(
+      connection,
+      pick(responseMessage, "messageSize", "correlationId"),
+    );
+  });
+
+  // const responseMessage = {
+  //   messageSize: Buffer.from(new Array(4).fill(0)),
+  //   correlationId: Buffer.from([...new Array(3).fill(0), 7]),
+  // };
+
+  // sendResponseMessage(connection, responseMessage);
 });
 
 server.listen(9092, "127.0.0.1");
