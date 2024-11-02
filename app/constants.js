fab5cb2ede20027f397987a4e7394afb7f3f88bd
Abhiram Pai <abhirampai1999@gmail.com>
2024-11-02 12:42:04 +0530

    Passing stage 'Include fetch in ApiVersions'

--- app/constants.js
+++ app/constants.js
@@ -1,9 +1,11 @@
-export const API_KEY_VERSIONS = [0, 1, 2, 3, 4];
+export const API_KEY_VERSIONS = [...Array(5).keys()];
 
 export const DESCRIBE_TOPIC_PARTITION_VERSIONS = [0];
 
+export const FETCH_API_VERSIONS = [...Array(17).keys()];
 
 export const API_KEYS = {
+  1: FETCH_API_VERSIONS,
   18: API_KEY_VERSIONS,
-  75: DESCRIBE_TOPIC_PARTITION_VERSIONS
-}
+  75: DESCRIBE_TOPIC_PARTITION_VERSIONS,
+};
