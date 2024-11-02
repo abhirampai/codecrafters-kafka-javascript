794fedeb4baed267f8620289b49ead7eb53d3e6a
Abhiram Pai <abhirampai1999@gmail.com>
2024-11-02 13:02:09 +0530

    Passing stage 'fetchApiRequest'

--- app/main.js
+++ app/main.js
@@ -5,6 +5,35 @@ import { handleApiVersionsRequest } from "./api_versions_request.js";
 import { parseRequest } from "./request_parser.js";
 import { handleDescribeTopicPartitionsRequest } from "./describe_topic_partitions_request.js";
 
+const handleFetchApiRequest = (connection, responseMessage, buffer) => {
+  const throttleTime = Buffer.from([0, 0, 0, 0]);
+  const errorCode = Buffer.from([0, 0]);
+  const sessionId = Buffer.from([0, 0, 0, 0]);
+  const responses = Buffer.from([1]);
+  const tagBuffer = Buffer.from([0]);
+
+  let fetchRequestResponse = {
+    correlationId: responseMessage.correlationId,
+    responseHeaderTagbuffer: tagBuffer,
+    throttleTime,
+    errorCode,
+    sessionId,
+    responses,
+    tagBuffer,
+  };
+
+  const messageSizeBuffer = Buffer.alloc(4);
+  messageSizeBuffer.writeInt32BE([
+    Buffer.concat(Object.values(fetchRequestResponse)).length,
+  ]);
+  fetchRequestResponse = {
+    messageSize: messageSizeBuffer,
+    ...fetchRequestResponse,
+  };
+
+  sendResponseMessage(connection, fetchRequestResponse);
+};
+
 const server = net.createServer((connection) => {
   connection.on("data", (buffer) => {
     const { messageSize, requestApiKey, requestApiVersion, correlationId } =
@@ -21,6 +50,8 @@ const server = net.createServer((connection) => {
 
     if (requestApiKey.readInt16BE() === 18) {
       handleApiVersionsRequest(connection, responseMessage, requestVersion);
+    } else if (requestApiKey.readInt16BE() === 1) {
+      handleFetchApiRequest(connection, responseMessage, buffer);
     } else if (requestApiKey.readInt16BE() === 75) {
       handleDescribeTopicPartitionsRequest(connection, responseMessage, buffer);
     } else {
